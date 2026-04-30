3e9849b8102b4ac45da84219d0e50bd7965bf901
PlotChat <levietanh1234567@gmail.com>
2026-04-30 18:13:15 +0700

    Update Button component
    Remove sizing for now and import styling for BaseUI

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -3,19 +3,19 @@ import * as Base from "@base-ui/react/button";
 import styles from "./Button.module.css";
 import { clsx } from "../../utils/clsx";
 
-type BtnVariant = "filled" | "hollow";
-type BtnSize = "sm" | "md" | "lg";
+type ButtonVariant = "filled" | "hollow";
+// type BtnSize = "sm" | "md" | "lg";
 
 interface ButtonProps extends Omit<React.ComponentPropsWithRef<typeof Base.Button>, "className"> {
     className?: string,
-	size?: BtnSize;
-	variant?: BtnVariant;
+	// size?: BtnSize;
+	variant?: ButtonVariant;
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-	({ size = "md", variant = "filled", className = "", children, ...rest }, ref) => {
+	({ variant = "filled", className = "", children, ...rest }, ref) => {
 		return (
-			<Base.Button {...rest} ref={ref} className={clsx(styles.btn, styles[size], styles[variant], className)}>
+			<Base.Button {...rest} ref={ref} className={clsx(styles.button, styles[variant], className)}>
 				{children}
 			</Base.Button>
 		);
