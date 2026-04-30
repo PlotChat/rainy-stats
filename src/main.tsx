d9e011bae3417eb4075a12274c39e2a07da658ad
PlotChat <levietanh1234567@gmail.com>
2026-04-30 18:20:27 +0700

    Add Home.tsx file for homepage. Add WidgetsProvider in Main.tsx for App to use currentWidgets

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './assets/global.css'
 import './assets/variables.css'
 import App from './App.tsx'
+import { WidgetsProvider } from './context/Widgets/WidgetsProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <WidgetsProvider>
+      <App />
+    </WidgetsProvider>
   </StrictMode>,
 )
