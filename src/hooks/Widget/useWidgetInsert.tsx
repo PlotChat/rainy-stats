c34dacf9390490332662e0585ea25a0baddce329
PlotChat <levietanh1234567@gmail.com>
2026-05-02 01:00:36 +0700

    Refactor: Separate useAddWidget functionality
    Split useAddWidget into useWidgetForm and useWidgetInsert, move widgets' events relating to adding widgets from App to useAddWidget

---
+++ src/hooks/Widget/useWidgetInsert.tsx
@@ -0,0 +1,39 @@
+import { useWidgetsDataContext } from "../../context/Widgets/WidgetsDataContext.tsx/WidgetsDataContext";
+import type { WidgetType } from "../../types/widget/WidgetType";
+
+const useWidgetInsert = () => {
+	const { setWidgets } = useWidgetsDataContext()
+
+	const insertWidget = (
+		widget: WidgetType,
+		uiIndex: number,
+		direction: "left" | "right",
+	) => {
+		setWidgets((cur) => {
+			const cleanWidgets = cur.filter((w) => !w?.isPreview);
+
+			// Because the UI index included the ghost widget, if the ghost
+			// was sitting *before* our target, our target just shifted left by 1
+			let actualIndex = uiIndex;
+			const ghostIndex = cur.findIndex((w) => w?.isPreview);
+
+			if (ghostIndex !== -1 && ghostIndex < uiIndex) {
+				actualIndex = uiIndex - 1;
+			}
+
+			// If clicking left, insert exactly at the actual index.
+			// If clicking right, insert right after the actual index.
+			const insertPosition =
+				direction === "left" ? actualIndex : actualIndex + 1;
+
+			const before = cleanWidgets.slice(0, insertPosition);
+			const after = cleanWidgets.slice(insertPosition);
+
+			return [...before, widget, ...after];
+		});
+	};
+
+	return { insertWidget };
+};
+
+export default useWidgetInsert;
