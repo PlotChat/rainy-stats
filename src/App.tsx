91c5d9a112bdbd99dc96a1e6fd7fea24a2e5971a
PlotChat <levietanh1234567@gmail.com>
2026-05-03 13:21:46 +0700

    Move all code from App to a Home component for splitting up pages
    Also add a placeholder message in case there are no children in the dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,12 @@
-import "./app.module.css";
-import appStyles from "./app.module.css";
-import mainStyles from "./main.module.css";
-import Dashboard from "./features/Dashboard/Dashboard";
-import { useWidgetsDataContext } from "./features/WidgetSelector/context/WidgetsDataContext.tsx/WidgetsDataContext";
-import Widget from "./components/widget/Widget";
-import useAddWidget from "./features/WidgetSelector/hooks/useAddWidget";
-import { AnimatePresence } from "framer-motion";
-import WidgetSelector from "./features/WidgetSelector/WidgetSelector";
+import styles from "./app.module.css";
+import Home from "./pages/home/Home";
 
 const App = () => {
-	const { widgets } = useWidgetsDataContext();
-	const { widgetsEdgesOnClick } = useAddWidget();
-
 	return (
-		<div className={appStyles.app}>
-			<WidgetSelector></WidgetSelector>
-
-			<main className={mainStyles.main}>
-				<Dashboard variant="grid" gridColumns={8}>
-					<AnimatePresence mode="popLayout">
-						{widgets.map((w, index) => (
-							<Widget
-								key={index}
-								widget={w}
-								widgetIndex={index}
-								onClickEdges={widgetsEdgesOnClick}
-							></Widget>
-						))}
-					</AnimatePresence>
-				</Dashboard>
-			</main>
+		<div className={styles.app}>
+			<Home></Home>
 		</div>
-	);
+	)
 };
 
 export default App;
