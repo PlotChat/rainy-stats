10f3245eae315683b4b3a88dd8c506bb05e595be
PlotChat <levietanh1234567@gmail.com>
2026-05-05 18:54:21 +0700

    Add more styling for button
    Add design tokens for button and apply an intent variable to switch between colors

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -4,18 +4,41 @@ import styles from "./Button.module.css";
 import { clsx } from "../../utils/clsx";
 
 type ButtonVariant = "filled" | "hollow";
+type ButtonIntent = "default" | "primary" | "danger" | "warning";
 // type BtnSize = "sm" | "md" | "lg";
 
-interface ButtonProps extends Omit<React.ComponentPropsWithRef<typeof Base.Button>, "className"> {
-    className?: string,
+interface ButtonProps extends Omit<
+	React.ComponentPropsWithRef<typeof Base.Button>,
+	"className"
+> {
+	className?: string;
 	// size?: BtnSize;
 	variant?: ButtonVariant;
+	intent?: ButtonIntent;
 }
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-	({ variant = "filled", className = "", children, ...rest }, ref) => {
+	(
+		{
+			variant = "filled",
+			intent = "default",
+			className = "",
+			children,
+			...rest
+		},
+		ref,
+	) => {
 		return (
-			<Base.Button {...rest} ref={ref} className={clsx(styles.button, styles[variant], className)}>
+			<Base.Button
+				{...rest}
+				ref={ref}
+				className={clsx(
+					styles.button,
+					intent !== "default" ? styles[intent] : "",
+					variant !== "filled" ? styles[variant]: "",
+					className,
+				)}
+			>
 				{children}
 			</Base.Button>
 		);
