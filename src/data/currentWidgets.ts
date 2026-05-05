635e16a08e55b9b8c5149b2536b44ad2e267c065
PlotChat <levietanh1234567@gmail.com>
2026-05-05 19:00:17 +0700

    Change currentWidgets for debug

--- src/data/currentWidgets.ts
+++ src/data/currentWidgets.ts
@@ -3,8 +3,8 @@ import type { WidgetListType } from "../types/WidgetListType";
 const currentWidgets: WidgetListType = [
 	{
 		type: "CardImage",
-		colSpan: 2,
-		rowSpan: 4,
+		colSpan: 3,
+		rowSpan: 3,
 		attribute: {
 			variant: "row",
 			title: "Hello there",
@@ -16,30 +16,42 @@ const currentWidgets: WidgetListType = [
 	},
 	{
 		type: "CardImage",
-		colSpan: 4,
-		rowSpan: 2,
+		colSpan: 3,
+		rowSpan: 3,
 		attribute: {
 			variant: "row",
 			title: "Hello there",
-			body: "This is a card",
+			body: "Hello thereHello thereHello thereHello thereHello thereHello",
 			src: "https://i.pinimg.com/736x/15/d4/d5/15d4d5329b30ef778ded24b1c7c713e1.jpg",
 			alt: "Flower",
 		},
 		isPreview: false,
-	},
-	{
+	},{
 		type: "CardImage",
 		colSpan: 2,
 		rowSpan: 2,
 		attribute: {
 			variant: "row",
 			title: "Hello there",
-			body: "This is a card",
+			body: "Hello thereHello thereHello thereHello thereHello thereHello",
+			src: "https://i.pinimg.com/736x/15/d4/d5/15d4d5329b30ef778ded24b1c7c713e1.jpg",
+			alt: "Flower",
+		},
+		isPreview: false,
+	},{
+		type: "CardImage",
+		colSpan: 2,
+		rowSpan: 2,
+		attribute: {
+			variant: "row",
+			title: "Hello there",
+			body: "Hello thereHello thereHello thereHello thereHello thereHello",
 			src: "https://i.pinimg.com/736x/15/d4/d5/15d4d5329b30ef778ded24b1c7c713e1.jpg",
 			alt: "Flower",
 		},
 		isPreview: false,
 	},
+
 ];
 
 export default currentWidgets;
