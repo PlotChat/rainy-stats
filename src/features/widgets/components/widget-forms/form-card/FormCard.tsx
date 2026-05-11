b6b066f0c7a11d56a6feb8262fd075aa67471333
PlotChat <levietanh1234567@gmail.com>
2026-05-11 10:49:46 +0700

    feat: add default values for FormCard

--- src/features/widgets/components/widget-forms/form-card/FormCard.tsx
+++ src/features/widgets/components/widget-forms/form-card/FormCard.tsx
@@ -1,37 +1,49 @@
-import type { FormProps } from "../../../../../components/form/Form";
 import Button from "../../../../../components/button/Button";
 import { clsx } from "../../../../../utils/clsx";
 import styles from "./form-card.module.css";
 import baseStyles from "../widget-forms.module.css";
 import Form from "../../../../../components/form/Form";
+import type { WidgetFormType } from "../types/widget-form-type";
 
-const FormCard = ({ className = "", onSubmit, action }: FormProps) => {
+const FormCard = ({
+	className = "",
+	onSubmit,
+	action,
+	selectedWidget,
+}: WidgetFormType) => {
+	const colSpan = selectedWidget?.colSpan || "";
+	const rowSpan = selectedWidget?.rowSpan || "";
+
+	const isCardImage = selectedWidget?.type === "Card";
+	const title = isCardImage ? selectedWidget.attribute.title : "";
+	const body = isCardImage ? selectedWidget.attribute.body : "";
+
 	return (
 		<Form
 			onSubmit={onSubmit}
 			className={clsx(className, styles.formCardImage, baseStyles.form)}
 			action={action}
 		>
-			<input type="hidden" name="type" value="Card"></input>
+			<input type="hidden" name="type" defaultValue="CardImage"></input>
 
 			<label htmlFor="title">
 				<p>Title</p>
-				<input type="text" name="title" required></input>
+				<input defaultValue={title} type="text" name="title" required></input>
 			</label>
 
 			<label htmlFor="content">
 				<p>Content</p>
-				<input type="text" name="content" required></input>
+				<input defaultValue={body} type="text" name="content" required></input>
 			</label>
 
 			<label htmlFor="rowSpan">
 				<p>Length</p>
-				<input type="number" name="rowSpan" required></input>
+				<input defaultValue={rowSpan} type="number" name="rowSpan" required></input>
 			</label>
 
 			<label htmlFor="colSpan">
 				<p>Height</p>
-				<input type="number" name="colSpan" required></input>
+				<input defaultValue={colSpan} type="number" name="colSpan" required></input>
 			</label>
 
 			<Button type="submit">Submit</Button>
