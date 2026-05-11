d83d233e6ed31550c99a27fe367fdca09d064c9b
PlotChat <levietanh1234567@gmail.com>
2026-05-11 09:33:44 +0700

    feat: add default values for editing for FormCardImage

--- src/features/widgets/components/widget-forms/form-card-image/FormCardImage.tsx
+++ src/features/widgets/components/widget-forms/form-card-image/FormCardImage.tsx
@@ -1,45 +1,58 @@
-import type { FormProps } from "../../../../../components/form/Form";
 import Button from "../../../../../components/button/Button";
 import { clsx } from "../../../../../utils/clsx";
 import styles from "./form-card-image.module.css";
-import baseStyles from '../widget-forms.module.css';
+import baseStyles from "../widget-forms.module.css";
 import Form from "../../../../../components/form/Form";
+import type { WidgetFormType } from "../types/widget-form-type";
 
-const FormCardImage = ({ className = "", onSubmit, action }: FormProps) => {
+const FormCardImage = ({
+	className = "",
+	onSubmit,
+	action,
+	selectedWidget,
+}: WidgetFormType) => {
+	const colSpan = selectedWidget?.colSpan || "";
+	const rowSpan = selectedWidget?.rowSpan || "";
+
+	const isCardImage = selectedWidget?.type === "CardImage";
+	const title = isCardImage ? selectedWidget.attribute.title : "";
+	const body = isCardImage ? selectedWidget.attribute.body : "";
+	const src = isCardImage ? selectedWidget.attribute.src : "";
+
 	return (
 		<Form
 			onSubmit={onSubmit}
 			className={clsx(className, styles.formCardImage, baseStyles.form)}
 			action={action}
 		>
-			<input type="hidden" name="type" value="CardImage"></input>
+			<input type="hidden" name="type" defaultValue="CardImage"></input>
 
 			<label htmlFor="title">
 				<p>Title</p>
-				<input type="text" name="title" required></input>
+				<input defaultValue={title} type="text" name="title" required></input>
 			</label>
 
 			<label htmlFor="content">
 				<p>Content</p>
-				<input type="text" name="content" required></input>
+				<input defaultValue={body} type="text" name="content" required></input>
 			</label>
 
 			<label htmlFor="src">
 				<p>Image Source</p>
-				<input type="text" name="src" required></input>
+				<input defaultValue={src} type="text" name="src" required></input>
 			</label>
 
 			<label htmlFor="rowSpan">
 				<p>Length</p>
-				<input type="number" name="rowSpan" required></input>
+				<input defaultValue={rowSpan} type="number" name="rowSpan" required></input>
 			</label>
 
 			<label htmlFor="colSpan">
 				<p>Height</p>
-				<input type="number" name="colSpan" required></input>
+				<input defaultValue={colSpan} type="number" name="colSpan" required></input>
 			</label>
 
-			<Button type="submit">Start Adding Card</Button>
+			<Button type="submit">Submit</Button>
 		</Form>
 	);
 };
