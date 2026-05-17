625bfed05d08066e3ca3454804f265688642d955
PlotChat <levietanh1234567@gmail.com>
2026-05-17 15:01:27 +0700

    feat: implement ITCSS into the project
    Refactored styling for components/ to follow ITCSS and add data-attributes

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -5,44 +5,27 @@ import { clsx } from "../../utils/clsx";
 
 type ButtonVariant = "filled" | "hollow";
 type ButtonIntent = "default" | "primary" | "danger" | "warning";
-// type BtnSize = "sm" | "md" | "lg";
 
-export interface ButtonProps extends Omit<
-	React.ComponentPropsWithRef<typeof Base.Button>,
-	"className"
-> {
-	className?: string;
-	// size?: BtnSize;
-	variant?: ButtonVariant;
-	intent?: ButtonIntent;
+export interface ButtonProps extends Omit<React.ComponentPropsWithRef<typeof Base.Button>, "className"> {
+    className?: string;
+    variant?: ButtonVariant;
+    intent?: ButtonIntent;
 }
 
 const Button = forwardRef<HTMLElement, ButtonProps>(
-	(
-		{
-			variant = "filled",
-			intent = "default",
-			className = "",
-			children,
-			...rest
-		},
-		ref,
-	) => {
-		return (
-			<Base.Button
-				{...rest}
-				ref={ref}
-				className={clsx(
-					styles.button,
-					intent !== "default" ? styles[intent] : "",
-					variant !== "filled" ? styles[variant]: "",
-					className,
-				)}
-			>
-				{children}
-			</Base.Button>
-		);
-	},
+    ({ variant = "filled", intent = "default", className = "", children, ...rest }, ref) => {
+        return (
+            <Base.Button
+                {...rest}
+                ref={ref}
+                className={clsx(styles.button, className)}
+                data-variant={variant}
+                data-intent={intent}
+            >
+                {children}
+            </Base.Button>
+        );
+    }
 );
 
 Button.displayName = "Button";
