a4a7d51895eb3b3ebcb6ef9c9762e271902c4673
PlotChat <levietanh1234567@gmail.com>
2026-05-19 18:54:22 +0700

    feat: pages now operate on a canvas breakout

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,31 +3,28 @@ import { useWidgetsDataContext } from "../../context/widgets-data-context/Widget
 import WidgetSelector from "../../features/widgets/components/widget-selector/WidgetSelector";
 import Widget from "../../features/widgets/components/widget/Widget";
 import Dashboard from "../../features/dashboard/Dashboard";
-import styles from "./Home.module.css";
 
 const Home = () => {
 	const { widgets } = useWidgetsDataContext();
 	const widgetIsEmpty = !widgets || widgets.length === 0;
 
 	return (
-		<div className={styles.Home}>
-			<main className={styles.main}>
-				<WidgetSelector></WidgetSelector>
-				<Dashboard
-					isEmpty={widgetIsEmpty}
-					emptyErrorMsg="There are no items in the dashboard. You can add some, though!"
-					variant="grid"
-					gridColumns={12}
-				>
-					{!widgetIsEmpty &&
-						widgets.map((w, index) => (
-							<AnimatePresence mode="popLayout" key={index}>
-								<Widget widget={w} widgetIndex={index}></Widget>
-							</AnimatePresence>
-						))}
-				</Dashboard>
-			</main>
-		</div>
+		<>
+			<WidgetSelector></WidgetSelector>
+			<Dashboard
+				isEmpty={widgetIsEmpty}
+				emptyErrorMsg="There are no items in the dashboard. You can add some, though!"
+				variant="grid"
+				gridColumns={12}
+			>
+				{!widgetIsEmpty &&
+					widgets.map((w, index) => (
+						<AnimatePresence mode="popLayout" key={index}>
+							<Widget widget={w} widgetIndex={index}></Widget>
+						</AnimatePresence>
+					))}
+			</Dashboard>
+		</>
 	);
 };
 
